69fd0cc34f69844488f13e78df2da5391bdf554b
Murilo <murilo.revisaprev@gmail.com>
2025-07-20 03:06:25 -0300

    ajuste

--- backend-rota/main.py
+++ backend-rota/main.py
@@ -1,27 +1,18 @@
 from fastapi import FastAPI, BackgroundTasks
-from fastapi.responses import FileResponse, JSONResponse, Response
+from fastapi.responses import FileResponse, JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from cpp_core import gerar_rota_cpp
 from pathlib import Path
 import uuid
 import os
-import requests
 from typing import Dict
-
-FCM_SERVER_KEY = "BEb8lSDu8z9f_ejV670IU_9gl9m7RpSKMwei-A1J9m4juMgj9gxzujJxM1PycsJxeMXJNph6CVzlKy61Q88YbKs"
 
 app = FastAPI()
 
-origins = [
-    "http://localhost:5173",
-    "https://murilomotomatsu.github.io",
-    "https://murilomotomatsu.github.io/RotasApi",
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
@@ -34,26 +25,7 @@ jobs: Dict[str, Dict] = {}
 class RotaInput(BaseModel):
     lat_lon: str
     raio_metros: float
-    fcm_token: str = None
     user_id: str = None
-
-def enviar_notificacao_fcm(token: str, job_id: str):
-    headers = {
-        "Authorization": f"key={FCM_SERVER_KEY}",
-        "Content-Type": "application/json"
-    }
-    payload = {
-        "to": token,
-        "notification": {
-            "title": "Rota finalizada!",
-            "body": "Seu trajeto solicitado já está pronto."
-        },
-        "data": {
-            "job_id": job_id
-        }
-    }
-    response = requests.post("https://fcm.googleapis.com/fcm/send", json=payload, headers=headers)
-    print("FCM status:", response.status_code, response.text)
 
 def processar_rota(uid: str, data: RotaInput):
     pasta_saida = f"static/rota_{uid}"
@@ -73,26 +45,14 @@ def processar_rota(uid: str, data: RotaInput):
             }
         })
 
-        if data.fcm_token:
-            enviar_notificacao_fcm(data.fcm_token, uid)
-
     except Exception as e:
         jobs[uid].update({"status": "erro", "erro": str(e)})
-
-@app.options("/rota")
-async def preflight_rota():
-    return Response(status_code=204, headers={
-        "Access-Control-Allow-Origin": "https://murilomotomatsu.github.io",
-        "Access-Control-Allow-Methods": "POST, OPTIONS",
-        "Access-Control-Allow-Headers": "*"
-    })
 
 @app.post("/rota")
 async def rota(data: RotaInput, background_tasks: BackgroundTasks):
     uid = str(uuid.uuid4())
     jobs[uid] = {
         "status": "em_andamento",
-        "fcm_token": data.fcm_token,
         "user_id": data.user_id
     }
     background_tasks.add_task(processar_rota, uid, data)
